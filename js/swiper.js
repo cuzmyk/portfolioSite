69df854fd3ff0c2330604759639b2b175a89e212
Mikhail Savelev <mixon749@gmail.com>
2023-02-02 13:14:07 +0300

    slider commit

--- js/swiper.js
+++ js/swiper.js
@@ -1,37 +1,29 @@
-// import Swiper from "swiper";
-// import "swiper/css";
-
-document.addEventListener("DOMContentLoaded", () => {
   const swiperImg = new Swiper(".projects__img", {
     loop: false,
     speed: 1500,
+    slidesPerView: 1,
     parallax: true,
+    grabCursor: true,
     navigation: {
       nextEl: ".project__btn.right",
       prevEl: ".project__btn.left",
     },
+    pagination: {
+      el: '.swiper-pagination',
+      type: 'bullets',
+      clickable: true,
+    }, 
   });
-});
-// let slider = new Swiper(".projects__slider", {
-//   loop: true,
-
-//   centeredSlides: true,
-//   slidesPerView: 2.07,
-//   spaceBetween: 30,
-//   slideToClickedSlide: true,
-//   parallax: true,
-
-//   effect: "slide",
-//   //   'slide' | 'fade' | 'cube' | 'coverflow' | 'flip' | 'creative' | 'cards'
-//   // grabCursor: true,
-//   speed: 500,
-//   navigation: {
-//     nextEl: ".swiper-button-next",
-//     prevEl: ".swiper-button-prev",
-//   },
-//   pagination: {
-//     el: ".swiper-pagination",
-//     type: "bullets",
-//     clickable: true,
-//   },
-// });
+  const swiperInfo = new Swiper(".projects__info", {
+    loop: false,
+    speed: 1500,
+    slidesPerView: 1,
+    allowTouchMove: false,
+    direction: 'vertical',
+  
+  });
+  
+  swiperImg.controller.control = swiperInfo
+  swiperInfo.controller.control = swiperImg
+ 
+ 
